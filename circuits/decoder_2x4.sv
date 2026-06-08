e36d97ad305f4f8967fa81ca58377318c8e55b32
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-06-08 00:33:24 -0300

    feat: add decoder 2x4

---
+++ circuits/decoder_2x4.sv
@@ -0,0 +1,18 @@
+module decoder_2x4(
+	input logic en,
+  	input logic [1:0] in,
+  	output logic [3:0] out
+);
+
+always_comb begin
+  out = 4'b0000;
+  if (en == 1'b1) begin
+    case (in)
+      2'b00: out = 4'b0001;
+      2'b01: out = 4'b0010;
+      2'b10: out = 4'b0100;
+      2'b11: out = 4'b1000;
+    endcase
+  end
+end
+endmodule
